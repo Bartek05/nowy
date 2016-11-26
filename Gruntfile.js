c96c201687861d75549dff87fffb6ef26f0b3f7c
Bartek Lichaj <Bartek Lichaj>
2016-11-26 20:15:39 +0100

    poprawka klamer w gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,13 +6,13 @@ module.exports = function(grunt) {
 	  sass: {
 	      options: {
 	        sourceMap: true
-	      },
+	      			},
 	      dist: {
 	        files: {
 	          'style.css': 'style.sass'
-	        }
-	      }
-	    }
+	        		}
+	      		}
+	    	}
 	  imagemin: {
 	    dynamic: {
 	        files: [{
@@ -20,33 +20,33 @@ module.exports = function(grunt) {
 	            cwd: 'images/',
 	            src: ['**/*.{png,jpg,gif}'],
 	            dest: 'images/build/'
-	        }]
-	    }
-	  }
+	        		}]
+	    		 }
+	  			}
 	  watch: {
     	scripts: {
         files: ['sass/*.sass'],
         tasks: ['sass'],
         options: {
             spawn: false,
-        },
-    } 
-	}
+        		 },
+    			} 
+			}
 	browserSync: {
 	    bsFiles: {
 	        src : 'assets/css/*.css'
-	    },
+	    		 },
 	    options: {
 	        server: {
 	            baseDir: "./"
-	        }
-	    }
-	}
+	        		}
+	    		 }
+				}
 	});
   // Load the plugins tasks 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-browser-sync');
   // Default task(s).
   grunt.registerTask('default', ['sass' 'imagemin' 'watch' 'browserSync']);
